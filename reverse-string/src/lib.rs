b807562cc920f1abd1d80627298de9f6365c08aa
Mayur Choudhary <mayur65@gmail.com>
2024-10-03 19:53:57 -0700

    Fix for exercise 2

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,4 +1,6 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    let rev_string = input.chars().rev().collect();
+    let rev_string = input.graphemes(true).rev().collect();
     return rev_string;
 }
